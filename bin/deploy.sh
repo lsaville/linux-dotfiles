b25017cd937b7e56e1181cb612d4875071ad7c5f
Lee Saville <lee.saville@americanfinancing.net>
2019-01-16 08:45:37 -0700

    Add frills

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,8 @@ if [ "$1" != "" ]; then
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
   expect -f deploy.exp $BRANCH $1
+  ./blink
+  ./slackpm jennmorris "Yo yo yo $BRANCH is deployed to $1"
 else
   echo "Missing the env arg"
 fi

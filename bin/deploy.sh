56aade8c28ec3d284968cd5239d527b9ba852729
Lee Saville <lee.saville@americanfinancing.net>
2019-01-18 07:38:18 -0700

    Fiddle with slack output

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,10 +2,11 @@
 
 if [ "$1" != "" ]; then
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  BRANCH_ABBRV=$(echo "$BRANCH" | cut -b -7 )
 
   expect -f deploy.exp $BRANCH $1
   ./blink
-  ./slackpm jennmorris "Yo yo yo $BRANCH is deployed to $1"
+  ./slackpm jennmorris "Yo yo yo @jennmorris $BRANCH_ABBRV is deployed to $1"
 else
   echo "Missing the env arg"
 fi

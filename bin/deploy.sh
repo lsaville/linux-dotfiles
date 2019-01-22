da8cc2113e1a7d2841490d435a04036051a0d6e2
Lee Saville <lee.saville@americanfinancing.net>
2019-01-22 14:49:39 -0700

    Set up script for conditional slack send but hit expect limitations

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [ "$1" != "" ]; then
-  BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  BRANCH_ABBRV=$(echo "$BRANCH" | cut -b -7 )
+if [ "$1" == "" ]; then echo "Missing the env arg"; exit 1; fi
 
-  expect -f deploy.exp $BRANCH $1
-  ./blink
-  ./slackpm jennmorris "Yo yo yo @jennmorris $BRANCH_ABBRV is deployed to $1"
-else
-  echo "Missing the env arg"
-fi
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH_ABBRV=$(echo "$BRANCH" | cut -b -7 )
+
+expect -f deploy.exp $BRANCH $1
+
+./blink
+./slackpm jennmorris "Yo yo yo @jennmorris $BRANCH_ABBRV is deployed to $1"
